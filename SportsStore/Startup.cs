c6d05f581f43d4e86ddd834c172c8c31555480c3
Jamie <jamiehdavis96@gmail.com>
2020-01-06 18:18:07 +0000

    Orders finished

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -27,6 +27,7 @@ namespace SportsStore
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddMemoryCache();
             services.AddSession();
