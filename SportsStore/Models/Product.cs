265951c6a19d443a8cc044794884ef55cee2d0fa
Jamie <jamiehdavis96@gmail.com>
2020-01-09 18:37:51 +0000

    jquery and stuff added

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace SportsStore.Models
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter a price")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Please enter a category")]
         public string Category { get; set; }
     }
 }
