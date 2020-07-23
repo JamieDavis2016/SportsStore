006082d5212b7dcfaff1475772892588eefded10
Jamie <jamiehdavis96@gmail.com>
2020-07-23 18:52:32 +0100

    ErrorController added

---
+++ SportsStore/Controllers/ErrorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Error() => View();
+    }
+}
